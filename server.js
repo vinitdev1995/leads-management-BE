c70c400ab904640ae6142ba2511438c227862a77
vinitdev1995 <vinitkumardevani@gmail.com>
2025-11-17 19:30:16 +0530

    Comment out production environment code in server.js for local development

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/leads', leadsRoutes);
 app.use('/api/auth', authRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build/index.html'));
-    });
-}
+//     app.get(/.*/, (req, res) => {
+//         res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//     });
+// }
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
